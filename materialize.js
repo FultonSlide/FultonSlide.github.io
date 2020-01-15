3791ea3b619843041705a2f9dea8511e74c9bdfb
Will <Wi_llo@hotmail.com>
2020-01-15 19:44:25 +1100

    folio site

---
+++ materialize.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+    $('.tabs').tabs();
+    $('.datepicker').datepicker();
+    $('.tooltipped').tooltip();
+    $('.scrollspy').scrollSpy();
+    $('.pushpin').pushpin();
+  });
